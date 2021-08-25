50617cc90717e96a4c6ab50269094b16eb415e77
deenhaque <fardeen2000@hotmail.com>
2021-08-25 17:26:25 -0400

    Added personal site github repo to projects, updated resume, potentially fixed gh-pages bug

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,29 +4,29 @@ import { extraCurriculars, projects, workExperience } from './projectData';
 import Text from '../Text/Text';
 
 const Projects = () => {
-	return (
-		<>
-			<br />
-			<Text>These are the places I've worked at:</Text>
-			<StyledProjects>
-				{workExperience.map((work, i) => (
-					<Project key={`${work.name}-${i}`} {...work} />
-				))}
-			</StyledProjects>
-			<Text>Outside of work, these are the guys I develop for:</Text>
-			<StyledProjects>
-				{extraCurriculars.map((extraCurricular, i) => (
-					<Project key={`${extraCurricular.name}-${i}`} {...extraCurricular} />
-				))}
-			</StyledProjects>
-			<Text>This is some cool stuff I've done:</Text>
-			<StyledProjects>
-				{projects.map((project, i) => (
-					<Project project key={`${project.name}-${i}`} {...project} />
-				))}
-			</StyledProjects>
-		</>
-	);
+  return (
+    <>
+      <br />
+      <Text>These are the places I've worked at:</Text>
+      <StyledProjects>
+        {workExperience.map((work, i) => (
+          <Project key={`${work.name}-${i}`} {...work} />
+        ))}
+      </StyledProjects>
+      <Text>Outside of work, these are the guys I develop for:</Text>
+      <StyledProjects>
+        {extraCurriculars.map((extraCurricular, i) => (
+          <Project key={`${extraCurricular.name}-${i}`} {...extraCurricular} />
+        ))}
+      </StyledProjects>
+      <Text>This is some cool stuff I've done:</Text>
+      <StyledProjects>
+        {projects.map((project, i) => (
+          <Project project key={`${project.name}-${i}`} {...project} />
+        ))}
+      </StyledProjects>
+    </>
+  );
 };
 
 export default Projects;
--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -1,83 +1,84 @@
 export const projects = [
-	{
-		name: 'Sundial',
-		tech: ['React Native', 'Flask'],
-		description:
-			'Sundial is a weather-aware, event planning mobile application.',
-		link: 'https://github.com/comp3004teamnumber1/sundial'
-	},
-	{
-		name: 'ImGhost',
-		tech: ['ReactJS', 'React Bootstrap'],
-		description:
-			'ImGhost is a (now taken down) public image hosting website that allows users could share any images with other users',
-		link: 'https://github.com/The-Homiesexuals/imghost'
-	},
-	{
-		name: 'Grade Calculator',
-		tech: ['ReactJS'],
-		description:
-			"Grade Calculator takes your grades and crunches the numbers for you to tell you how you're doing and what you need to get the grade you want.",
-		link: 'https://github.com/The-Homiesexuals/imghost'
-	},
-	{
-		name: 'Personal Website',
-		tech: ['ReactJS', 'Styled Components'],
-		description: 'Hope you like this project, haha.'
-	},
-	{
-		name: 'The Tortoise and the Hare: Dragon Edition',
-		tech: ['C++'],
-		description:
-			'A console sim which randomly spawns a hare and a tortoise has them run through a obstacle course to beat a dragon at the end.'
-	},
-	{
-		name: 'Website Certification Checker',
-		tech: ['JavaScript', 'HTML', 'CSS'],
-		description:
-			"A browser extension which tells the user how certified the website they're on is."
-	}
+  {
+    name: 'Sundial',
+    tech: ['React Native', 'Flask'],
+    description:
+      'Sundial is a weather-aware, event planning mobile application.',
+    link: 'https://github.com/comp3004teamnumber1/sundial',
+  },
+  {
+    name: 'ImGhost',
+    tech: ['ReactJS', 'React Bootstrap'],
+    description:
+      'ImGhost is a (now taken down) public image hosting website that allows users could share any images with other users',
+    link: 'https://github.com/The-Homiesexuals/imghost',
+  },
+  {
+    name: 'Grade Calculator',
+    tech: ['ReactJS'],
+    description:
+      "Grade Calculator takes your grades and crunches the numbers for you to tell you how you're doing and what you need to get the grade you want.",
+    link: 'https://github.com/The-Homiesexuals/imghost',
+  },
+  {
+    name: 'Personal Website',
+    tech: ['ReactJS', 'Styled Components'],
+    description: 'Hope you like this project, haha.',
+    link: 'https://github.com/deenhaque/Personal-Site/',
+  },
+  {
+    name: 'The Tortoise and the Hare: Dragon Edition',
+    tech: ['C++'],
+    description:
+      'A console sim which randomly spawns a hare and a tortoise has them run through a obstacle course to beat a dragon at the end.',
+  },
+  {
+    name: 'Website Certification Checker',
+    tech: ['JavaScript', 'HTML', 'CSS'],
+    description:
+      "A browser extension which tells the user how certified the website they're on is.",
+  },
 ];
 
 export const extraCurriculars = [
-	{
-		name: 'Organizer/Developer',
-		organization: 'cuHacking',
-		tech: ['ReactJS', 'TypeScript', 'Electron'],
-		description:
-			"I'm developing a map editing tool that allows users to input geojson data to build create maps.",
-		link: ''
-	},
-	{
-		name: 'Front End Developer',
-		organization: 'SpaceLab',
-		tech: ['ReactJS', 'SCSS'],
-		description:
-			"I'm working on the SpaceLab website with the other SpaceLab developers!",
-		link: ''
-	}
+  {
+    name: 'Organizer/Developer',
+    organization: 'cuHacking',
+    tech: ['ReactJS', 'TypeScript', 'Electron'],
+    description:
+      "I'm developing a map editing tool that allows users to input geojson data to build create maps.",
+    link: '',
+  },
+  {
+    name: 'Front End Developer',
+    organization: 'SpaceLab',
+    tech: ['ReactJS', 'SCSS'],
+    description:
+      "I'm working on the SpaceLab website with the other SpaceLab developers!",
+    link: '',
+  },
 ];
 
 export const workExperience = [
-	{
-		name: 'Front End Developer',
-		organization: 'Slice Labs',
-		tech: ['ReactJS', 'Flow'],
-		description:
-			"I'm creating and maintaining the user facing aspect of Slice products!"
-	},
-	{
-		name: 'Software Developer',
-		organization: 'Ross Video',
-		tech: ['C', 'GTK'],
-		description:
-			'I worked with the Switcher team to write features and fix bugs for the switcher simulator.'
-	},
-	{
-		name: 'Server Side Cloud Developer',
-		organization: 'Enghouse Espial Inc.',
-		tech: ['JavaScript', 'Mocha', 'AWS'],
-		description:
-			"I created new features, maintained existing ones, and automated testing for it all to ensure Espial's Elevate product works flawlessly."
-	}
+  {
+    name: 'Front End Developer',
+    organization: 'Slice Labs',
+    tech: ['ReactJS', 'Flow'],
+    description:
+      "I'm creating and maintaining the user facing aspect of Slice products!",
+  },
+  {
+    name: 'Software Developer',
+    organization: 'Ross Video',
+    tech: ['C', 'GTK'],
+    description:
+      'I worked with the Switcher team to write features and fix bugs for the switcher simulator.',
+  },
+  {
+    name: 'Server Side Cloud Developer',
+    organization: 'Enghouse Espial Inc.',
+    tech: ['JavaScript', 'Mocha', 'AWS'],
+    description:
+      "I created new features, maintained existing ones, and automated testing for it all to ensure Espial's Elevate product works flawlessly.",
+  },
 ];
