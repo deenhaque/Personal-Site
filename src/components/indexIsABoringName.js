5914bdf5ad5fd8b731c0932a4eabce0887165b03
deenhaque <fardeen2000@hotmail.com>
2021-08-25 16:11:34 -0400

    Updates

--- src/components/indexIsABoringName.js
+++
@@ -1,6 +0,0 @@
-export { default as About } from './About/About';
-export { default as App } from './App/App';
-export { default as Footer } from './Footer/Footer';
-export { default as Header } from './Header/Header';
-export { default as Projects } from './Projects/Projects';
-export { default as Text } from './Text/Text';
